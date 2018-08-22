4ee41b7abd4e606bcc176ec5638ea16658770e8a
Kathleen Vincent <kvincen4316@gmail.com>
2018-08-22 13:43:23 -0500

    styling livefeed page

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = ({ children }) => (
   <div
-    style={{ height: 100, clear: "both", paddingTop: 25, textAlign: "center" }}
+    style={{ height: 200, clear: "both", paddingTop: 25, textAlign: "center" }}
     className="TunedIn"
   >
     {children}
--- client/src/pages/Books/Books.js
+++ client/src/pages/Books/Books.js
@@ -7,6 +7,13 @@ import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 
+const style = {
+  pic: {
+    width: 64,
+    height: 64
+  }
+}
+
 class Books extends Component {
   state = {
     books: [],
@@ -56,42 +63,46 @@ class Books extends Component {
   render() {
     return (
       <Container fluid>
+        <Jumbotron>
+          <h1>TunedUp</h1>
+        </Jumbotron>
         <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>Welcome to tunedIn</h1>
-            </Jumbotron>
-            <form>
-              <Input
-                value={this.state.title}
-                onChange={this.handleInputChange}
-                name="title"
-                placeholder="Title"
-              />
-              <Input
-                value={this.state.author}
-                onChange={this.handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                value={this.state.contact}
-                onChange={this.handleInputChange}
-                name="contact"
-                placeholder="Contact Info (required)"
-              />
-              <FormBtn
-                disabled={!(this.state.author && this.state.title)}
-                onClick={this.handleFormSubmit}
-              >
-                post it!
-              </FormBtn>
-            </form>
+          <Col size="md-3">
+            <Container fluid>
+              <div className=" shadow-lg p-3 mb-5 bg-white rounded">
+                <img className="rounded" src="https://images.unsplash.com/photo-1509222796416-4a1fef025e92?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=66b0cbea122ce95175f28738ac3d0719&auto=format&fit=crop&w=700&q=60" style={style.pic} alt="Generic placeholder image"/>
+                <h4>Jane Doe</h4>
+                <hr></hr>
+                <p><strong>Instrument: </strong>Flute</p>
+                <p><strong>Genre: </strong>Classical</p>
+              </div>
+
+
+            </Container>
           </Col>
-          <Col size="md-12 sm-12" class="postJumbo">
-            <Jumbotron>
-              <h1>Posts</h1>
-            </Jumbotron>
+          <Col size="md-1"></Col>
+          <Col size="md-8">
+            <div className="media">
+              <img className="mr-3" src="https://images.unsplash.com/photo-1521503862198-2ae9a997bbc9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd180d123f5a072662e6f1cfb64bbde3&auto=format&fit=crop&w=700&q=60" style={style.pic} alt="Generic placeholder image"/>
+              <div className="media-body">
+                <h5 className="mt-0"> 
+                <span><Input value={this.state.title} onChange={this.handleInputChange} name="title" placeholder="Title"/></span></h5>
+                <TextArea value={this.state.contact} onChange={this.handleInputChange} name="listing" placeholder="What's your shout-out? (required)"/>
+                <FormBtn disabled={!(this.state.author && this.state.title)} onClick={this.handleFormSubmit}>post it!</FormBtn>
+              </div>
+            </div>
+            <Row></Row>
+            <Row>
+            <div className="media">
+              <img className="mr-3" src="https://images.unsplash.com/photo-1517430529647-90cda5b40093?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c0c3c22799cb1acffee5bc833906df8&auto=format&fit=crop&w=700&q=60" style={style.pic} alt="Generic placeholder image"/>
+                <div className="media-body">
+                  <h5 className="mt-0">French horn needed</h5>
+                   Looking for french horn to play at upcoming recital at the University of Minnesota.
+                </div>
+            </div>
+            </Row>
+
+            <Row>
             {this.state.books.length ? (
               <List>
                 {this.state.books.map(book => (
@@ -110,10 +121,13 @@ class Books extends Component {
               </List>
             ) : (
                 <h3>No Posts to Display</h3>
-              )}
+              )}  
+            </Row>
+
           </Col>
         </Row>
       </Container>
+
     );
   }
 }
