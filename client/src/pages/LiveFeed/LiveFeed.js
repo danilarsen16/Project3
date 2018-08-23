7aef7d8a1a5e150e4eeff60f7fbcab1c44d75f2c
Kathleen Vincent <vince115@umn.edu>
2018-08-23 14:19:48 -0500

    styling profile (#19)

--- client/src/pages/LiveFeed/LiveFeed.js
+++ client/src/pages/LiveFeed/LiveFeed.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 
-const style = {
-  pic: {
-    width: 64,
-    height: 64
-  }
-}
 
 class LiveFeed extends Component {
   state = {
@@ -64,26 +55,24 @@ class LiveFeed extends Component {
     return (
       <Container fluid>
         <Jumbotron>
-          <h1>TunedUp</h1>
+          <h1><span><img src="levels.png"/></span>TunedUp</h1>
         </Jumbotron>
         <Row>
           <Col size="md-3">
             <Container fluid>
-              <div className=" shadow-lg p-3 mb-5 bg-white rounded">
-                <img className="rounded" src="https://images.unsplash.com/photo-1509222796416-4a1fef025e92?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=66b0cbea122ce95175f28738ac3d0719&auto=format&fit=crop&w=700&q=60" style={style.pic} alt="Generic placeholder image"/>
+              <div className>
+                <img className="rounded" src="https://images.unsplash.com/photo-1509222796416-4a1fef025e92?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=66b0cbea122ce95175f28738ac3d0719&auto=format&fit=crop&w=700&q=60" style={{ width: 64, height: 64 }} alt="Generic placeholder image"/>
                 <h4>Jane Doe</h4>
                 <hr></hr>
                 <p><strong>Instrument: </strong>Flute</p>
                 <p><strong>Genre: </strong>Classical</p>
               </div>
-
-
             </Container>
           </Col>
           <Col size="md-1"></Col>
           <Col size="md-8">
             <div className="media">
-              <img className="mr-3" src="https://images.unsplash.com/photo-1521503862198-2ae9a997bbc9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd180d123f5a072662e6f1cfb64bbde3&auto=format&fit=crop&w=700&q=60" style={style.pic} alt="Generic placeholder image"/>
+              <img className="mr-3" src="https://images.unsplash.com/photo-1521503862198-2ae9a997bbc9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd180d123f5a072662e6f1cfb64bbde3&auto=format&fit=crop&w=700&q=60" style={{ width: 64, height: 64 }} alt="Generic placeholder image"/>
               <div className="media-body">
                 <h5 className="mt-0"> 
                 <span><Input value={this.state.title} onChange={this.handleInputChange} name="title" placeholder="Title"/></span></h5>
@@ -94,34 +83,12 @@ class LiveFeed extends Component {
             <Row></Row>
             <Row>
             <div className="media">
-              <img className="mr-3" src="https://images.unsplash.com/photo-1517430529647-90cda5b40093?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c0c3c22799cb1acffee5bc833906df8&auto=format&fit=crop&w=700&q=60" style={style.pic} alt="Generic placeholder image"/>
+              <img className="mr-3" src="https://images.unsplash.com/photo-1517430529647-90cda5b40093?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c0c3c22799cb1acffee5bc833906df8&auto=format&fit=crop&w=700&q=60" style={{ width: 64, height: 64 }} alt="Generic placeholder image"/>
                 <div className="media-body">
                   <h5 className="mt-0">French horn needed</h5>
                    Looking for french horn to play at upcoming recital at the University of Minnesota.
                 </div>
             </div>
-            </Row>
-
-            <Row>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                  {/* =======================below is the link to get to users profile for contact info ==========================*/}
-                    <Link to={"/books/" + book._id}>
-                      <font size="5" color="salmon"><strong>{book.title}</strong></font>
-                      <br />
-                      <br />
-                      <font color="grey">Contact <strong>{book.author} </strong>@ {book.contact}</font>
-                      {/* <a href="mailto:{book.contact}">Email Me</a>  */}
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-                <h3>No Posts to Display</h3>
-              )}  
             </Row>
 
           </Col>
@@ -132,4 +99,4 @@ class LiveFeed extends Component {
   }
 }
 
-export default Books;
+export default LiveFeed;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,41 +1,73 @@
 import React from "react";
-import Row from "./components/Row";
-import Col from "./components/Col";
-import { Card, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
-import Image from 'react-image-resizer';
+import { Col, Row, Container } from "../../components/Grid";
+import "./Profile.css";
 
-const Profile = () => {
-  
-  return (
-    <div>
+const Profile = () => (
+  <div>
+    <Container fluid>
+    <div class="bio">
       <Row>
-      <Col size="md-4">
-      <Card>
-        <Image top width="100%" src="https://www.biography.com/.image/t_share/MTQ1MTM5ODM2NTQ0ODIwNjMy/alicia-keys---mini-biography.jpg" alt="Card image cap" 
-        height={180}
-        width={318}/>
-        <CardBody>
-          <CardTitle>Alicia Keys</CardTitle>
-          <CardSubtitle>New York, NY</CardSubtitle>
-          <CardText>Instruments: Vocals, Piano</CardText>
-          <CardText>Genres: R&B, Hip-Hop, Jazz, Soul</CardText>
-          <Button>SoundCloud</Button>
-          <Button>YouTube</Button>
-        </CardBody>
-      </Card>
-      </Col>
-
-      <Col size="md-8">
-      <h2>Bio</h2>
-        <p>Alicia Keys was born in Hell's Kitchen, Manhattan, to Teresa (Augello), a paralegal who was also an occasional actress, and Craig Cook, a flight attendant.
-          Her father is African-American, and her mother, who is caucasian, is of Italian and English/Irish/Scottish ancestry. 
-      </p>
-      </Col>
+        <Col size="md-3">
+          <div>
+            <img class="rounded float-right" src="https://is5-ssl.mzstatic.com/image/thumb/Music62/v4/a0/b9/3a/a0b93aa4-0282-e495-6cef-6aae7340e280/886446159805.jpg/268x0w.jpg" style={{ width: 150, height: 150 }} alt="Generic user picture"/>
+          </div>
+        </Col>
+        <Col size="md-5">
+          <Container fluid>
+          <div>
+            <h3>Alicia Keys</h3>
+              <p><span><img src="placeholder.png" style={{width: 20, height: 20}}/> New York, NY</span></p>
+              <p><strong>Instruments: </strong>Vocals Piano</p>
+              <p><strong>Genre: </strong>R&B, Jazz, Hip-Hop, Soul</p>
+          </div>
+          </Container>
+        </Col>
       </Row>
-    </div>
-  );
-};
+      </div> 
+    </Container>
+    <Container fluid>
+      <Row>
+      <Col size="md-8">
+        <Container fluid>
+        <div class="shadow rounded top">
+          <h5>ABOUT ME</h5>
+          <hr></hr>
+          <p>Alicia Augello Cook was born on January 25, 1981, in the Hell's Kitchen neighborhood of New York City’s Manhattan borough. She is the only child of Teresa (Augello), a paralegal and part-time actress, and one of three children of Craig Cook, a flight attendant.Key's father is African American and her mother is of Sicilian (Agrigento and Sciacca) and either Scottish or Irish descent. Named after her Puerto Rican godmother, Keys expressed that she was comfortable with her multiracial heritage because she felt she was able to relate to different cultures.</p>
+        </div>
+        </Container>
 
+        <Container fluid>
+        <div class="shadow rounded">
+          <h5>MY POSTINGS</h5>
+          <hr></hr>
+          <div className="media">
+            <img className="mr-3" src="https://images.unsplash.com/photo-1517430529647-90cda5b40093?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c0c3c22799cb1acffee5bc833906df8&auto=format&fit=crop&w=700&q=60" style={{ width: 64, height: 64 }} alt="Generic placeholder image"/>
+              <div className="media-body">
+                <h5 className="mt-0">French horn needed</h5>
+                  Looking for french horn to play at upcoming recital at the University of Minnesota.
+            </div>
+          </div>
+        </div>
+        </Container>
+      </Col>
+
+      <Col size="md-4">
+      <Container fluid>
+        <div class="shadow rounded top">
+          <h6>Contact & Personal Info</h6>
+          <hr></hr>
+          <p><span><img src="email.png" style={{width: 20, height: 20}}/> email@email.com</span></p>
+          <p><span><img src="phone.png" style={{width: 20, height: 20}}/> xxx-xxx-xxxx</span></p>
+        </div>
+        </Container>
+      </Col>
+
+      </Row>
+      
+
+
+    </Container>
+  </div>
+);
 
 export default Profile;
