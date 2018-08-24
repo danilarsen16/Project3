f23fbba4fef31b331fad74a455a321811d7ddfbf
S. Funkenbusch <36971616+LefseTime@users.noreply.github.com>
2018-08-24 14:34:51 -0500

    changes so stuff doesnt break in production (#29)

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,18 +2,17 @@ import axios from "axios";
 
 export default {
 
-
-//   createUser: function (id){
-//     return axios.post("/api/user" + id, userData);
-//   },
-//   getUser: function(id){
-//     return axios.get("/api/user" + id, userData);
-//   },
-//   updateUser: function(id){
-//     return axios.update("api/user" + id);
-//   },
-  getListings: function(){
-    return axios.get("/api/listings");
+  createUser: function (id){
+    return axios.post("/api/" + id );
+  },
+  getUser: function(id){
+    return axios.get("/api/" + id );
+  },
+  updateUser: function(id){
+    return axios.update("api/" + id);
+  },
+  getListings: function(id){
+    return axios.get("api/listings" + id);
   },
   createListings: function(){
     return axios.post("/api/createListings");
