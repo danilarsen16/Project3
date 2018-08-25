a4f66474f75339035575795c6065988cb90bdeab
Marissa <marissa.ellingson@gmail.com>
2018-08-25 15:07:07 -0500

    formatted livefeed

--- client/src/pages/LiveFeed/LiveFeed.js
+++ client/src/pages/LiveFeed/LiveFeed.js
@@ -62,19 +62,11 @@ class LiveFeed extends Component {
         <Row>
           <Col size="md-3">
             <Container fluid>
-              <div className>
-                <img className="rounded" src="https://images.unsplash.com/photo-1509222796416-4a1fef025e92?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=66b0cbea122ce95175f28738ac3d0719&auto=format&fit=crop&w=700&q=60" style={{ width: 64, height: 64 }} alt="Generic placeholder image" />
-                <h4>Jane Doe</h4>
-                <hr></hr>
-                <p><strong>Instrument: </strong>Flute</p>
-                <p><strong>Genre: </strong>Classical</p>
-              </div>
             </Container>
           </Col>
           <Col size="md-1"></Col>
           <Col size="md-8">
             <div className="media">
-              <img className="mr-3" src="https://images.unsplash.com/photo-1521503862198-2ae9a997bbc9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd180d123f5a072662e6f1cfb64bbde3&auto=format&fit=crop&w=700&q=60" style={{ width: 64, height: 64 }} alt="Generic placeholder image" />
               <div className="media-body">
                 <h5 className="mt-0">
                   <span><Input value={this.state.title} onChange={this.handleInputChange} name="title" placeholder="Title" /></span></h5>
@@ -83,44 +75,18 @@ class LiveFeed extends Component {
               </div>
             </div>
             <Row></Row>
-
-
-            <Row>
-              <div className="media">
-                <img className="mr-3" src="https://images.unsplash.com/photo-1517430529647-90cda5b40093?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c0c3c22799cb1acffee5bc833906df8&auto=format&fit=crop&w=700&q=60" style={{ width: 64, height: 64 }} alt="Generic placeholder image" />
-                <div className="media-body">
-                  <h5 className="mt-0">French horn needed</h5>
-                  Looking for french horn to play at upcoming recital at the University of Minnesota.
-                </div>
-              </div>
-            </Row>
-
             <Row>
               {this.state.listings.length ? (
                 <List>
                   {this.state.listings.map(listings => {
-                                  //       <ListItem key={listing._id}>
-                                  //       {/* =======================below is the link to get to users profile for contact info ==========================*/}
-                                  //       <Link to={"/listings/" + listing._id}>
-                                  //         <font size="5" color="salmon"><strong>{listing.title}</strong></font>
-                                  //         <br />
-                                  //         <br />
-                                  //         <font color="grey">description <strong>{listing.user_id} </strong>@ {listing.description}</font>
-                                  //         {/* <a href="mailto:{listing.contact}">Email Me</a>  */}
-                                  //       </Link>
-                                  //       <DeleteBtn onClick={() => this.deleteListing(listing._id)} />
-                                  //     </ListItem>
-                                  //   ))}
-                                  // </List>
                     return (
                     <ListItem key={listings._id}>
-                    <a href={"/listings" + listings._id}>
-                    <strong>
-                      {listings.title} {listings.description}
-                      </strong>
-                      </a>
-
+                    <h2>
+                      {listings.title} 
                       <DeleteBtn onClick={() => this.deleteListings(listings._id)} />
+                      </h2>
+                      <strong>Posted By: {listings.username}</strong>
+                      <p>{listings.description}</p>
                     </ListItem>
                     );
                   })}
