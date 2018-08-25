21ba96e7766cc0a442969e307db4c9d22d2d3dd5
Marissa E <35227935+maellingson@users.noreply.github.com>
2018-08-25 15:16:46 -0500

    Styled livefeed (#31)
    * fixed react issues
    
    * Server and File Updates
    
    * formatted livefeed

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,17 +3,17 @@ import axios from "axios";
 export default {
 
 
-  createUser: function (id){
-    return axios.post("/api/user" + id );
-  },
-  getUser: function(id){
-    return axios.get("/api/user" + id );
-  },
-  updateUser: function(id){
-    return axios.update("api/user" + id);
-  },
-  getListings: function(id){
-    return axios.get("api/listings" + id);
+//   createUser: function (id){
+//     return axios.post("/api/user" + id, userData);
+//   },
+//   getUser: function(id){
+//     return axios.get("/api/user" + id, userData);
+//   },
+//   updateUser: function(id){
+//     return axios.update("api/user" + id);
+//   },
+  getListings: function(){
+    return axios.get("/api/listings");
   },
   createListings: function(){
     return axios.post("/api/createListings");
