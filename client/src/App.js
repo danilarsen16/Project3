1a4addb9ac9ef273566decaf8612b1a3f654ab74
Kathleen Vincent <vince115@umn.edu>
2018-08-27 11:25:33 -0500

    Edit btn (#41)
    * fixed edit butten on profile edit page
    
    * profile edit pages updating and redirecting

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,37 @@ import API from "./utils/API";
 class App extends Component {
   state = {
     googleuser: {},
+    profile: {
+      username: "Alicia Keys",
+      location: "New York, NY",
+      instruments: "Vocals, Piano",
+      genres: "R&B, Jazz, Hip-Hop, Soul",
+      bio: "Alicia Augello Cook was born on January 25, 1981, in the Hell's Kitchen neighborhood of New York City’s Manhattan borough. She is the only child of Teresa (Augello), a paralegal and part-time actress, and one of three children of Craig Cook, a flight attendant.Key's father is African American and her mother is of Sicilian (Agrigento and Sciacca) and either Scottish or Irish descent. Named after her Puerto Rican godmother, Keys expressed that she was comfortable with her multiracial heritage because she felt she was able to relate to different cultures.",
+
+      email: "aliciakeys@email.com",
+      phone: "xxx-xxx-xxxx",
+      image: "https://images.unsplash.com/photo-1517430529647-90cda5b40093?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9c0c3c22799cb1acffee5bc833906df8&auto=format&fit=crop&w=700&q=60",
+
+      listings: [],
+
+    }
   }
 
   componentDidMount() {
-    API.getGoogleUser()
+    // API.getGoogleUser()
+    //   .then(res =>
+    //     //console.log('API RES',res.data)
+    //     //this.setState({googleuser: res.data})
+    //     //console.log(res.data)
+    //     this.setState({ googleuser: res.data })
+    //   )
+    //   .catch(err => console.log(err));
+    //this.setState({_id: req.user[0]._id})
+    this.loadProfile()
+  }
+
+    loadProfile = () => {
+      API.getGoogleUser()
       .then(res =>
         //console.log('API RES',res.data)
         //this.setState({googleuser: res.data})
@@ -21,8 +48,7 @@ class App extends Component {
         this.setState({ googleuser: res.data })
       )
       .catch(err => console.log(err));
-    //this.setState({_id: req.user[0]._id})
-  }
+    };
 
   render() {
     return (
@@ -34,8 +60,8 @@ class App extends Component {
             <Route exact path="/" render={(props) => <LiveFeed {...props} googleuser={this.state.googleuser} />} />
             {/* //googleuser={this.state.googleuser} component={LiveFeed} /> */}
             <Route exact path="/livefeed" render={(props) => <LiveFeed {...props} googleuser={this.state.googleuser} />} />
-            <Route exact path="/myprofile" render={(props) => <Profile {...props} googleuser={this.state.googleuser} />} />
-            <Route exact path="/myprofile/edit" render={(props) => <ProfileEdit {...props} googleuser={this.state.googleuser} />} />
+            <Route exact path="/myprofile" render={(props) => <Profile {...props} profile={this.state.googleuser} googleuser={this.state.googleuser} />} />
+            <Route exact path="/myprofile/edit" render={(props) => <ProfileEdit {...props} updateProfile={this.loadProfile} googleuser={this.state.googleuser} />} />
             <Route path="/users" render={(props) => <OtherUserProfile {...props} googleuser={this.state.googleuser} />} />
 
             {/* <Route exact path="/myprofile/edit" component={ProfileEdit} />
